ce5dcf22195795ee91a03f549a926d14ddf31723
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-04-11 10:55:03 +0530

    Update issue Probably Solved Now

--- app/api/put/route.js
+++ app/api/put/route.js
@@ -1,13 +1,15 @@
-export const dynamic = 'force-dynamic'
+
 import prisma from "../../../lib/prisma";
 import { NextResponse } from "next/server";
 
-const PUT= async (req, res) => {
 
+
+
+export const dynamic = 'force-dynamic'
+const PUT= async (req, res) => {
   console.log("we are here to update")
   try {
     const { todo_id, status} = await req.json();
-
     const updateTodo = await prisma.todo.update({
       where: {
         todo_id: todo_id,
--- app/todo/components/TodoCards.jsx
+++ app/todo/components/TodoCards.jsx
@@ -29,7 +29,7 @@ deleteTodo("api/delete",todo_id)
         Description:{props.desc}
       </p>
 
-      <section className="flex flex-col w-[40%]">
+      <section className="flex flex-col w-[70%]">
       <select
             id="countries"
             className="bg-gray-50 my-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
